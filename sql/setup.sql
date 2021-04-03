5717b4c3c8c19b7f1ff3b0f143761ba82f913d4b
Dominique Sakrisson <domsakrisson@gmailcom>
2021-04-02 18:02:37 -0700

    add item class

--- sql/setup.sql
+++ sql/setup.sql
@@ -7,3 +7,10 @@ CREATE TABLE orders (
   item_price DOUBLE PRECISION,
   quantity INTEGER CHECK (quantity > 0)
 )
+CREATE TABLE items (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  item_id VARCHAR(6),
+  item_name VARCHAR(50),
+  item_price DOUBLE PRECISION,
+  quantity INTEGER CHECK (quantity > 0)
+)
