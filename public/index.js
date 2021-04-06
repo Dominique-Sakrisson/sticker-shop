8b25698899f82dc1b0d1f24d0ea9a11e559de472
Dominique Sakrisson <domsakrisson@gmailcom>
2021-04-05 20:06:52 -0700

    POST/GET ORDERS, ADD TO S3 BUCKET

---
+++ public/index.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('create-product');
+const ul = document.getElementById('products');
+
+ const appendItem = (item) => {
+     const li = document.createElement('li');
+     li.textContent = `${item.itemName} - ${item.itemPrice}`
+     ul.appendChild(li);
+ };
+
+ form.addEventListener('submit', (event) => {
+     event.preventDefault();
+     const data = new FormData(form);
+    console.log(data);
+     fetch('http://localhost:7890/api/v1/orders/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            itemId: Math.round((Math.random() * 20)),
+            itemName: data.get('itemName'),
+            itemPrice: data.get('itemPrice'),
+            quantity: data.get('quantity'),
+        }),
+      })
+        .then((res) => res.json())
+        .then(appendItem);
+ });
+
+ fetch('http://localhost:7890/api/v1/orders')
+    .then((res) => res.json())
+    .then((items) => {
+    items.forEach(appendItem);
+ });
